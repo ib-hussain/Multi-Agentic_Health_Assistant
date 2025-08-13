33bcec480083457dcc1608318de2992a4802a79d
ib-hussain <ibrahimbeaconarion@gmail.com>
2025-08-13 06:20:04 +0500

    addded ui effects

--- chatbots/reasoning.py
+++ chatbots/reasoning.py
@@ -1,4 +1,4 @@
 def respond(stringi):
-    print(f"**{stringi}**\n hey  so the thing is that as of yet i have just integrated the picture analysis chatbot yet, but i am working on integrating the other chatbots as well dont worry ")
+    # print(f"**{stringi}**\n hey  so the thing is that as of yet i have just integrated the picture analysis chatbot yet, but i am working on integrating the other chatbots as well dont worry ")
     a= f"**{stringi}**\n hey  so the thing is that as of yet i have just integrated the picture analysis chatbot yet, but i am working on integrating the other chatbots as well dont worry "
     return a
